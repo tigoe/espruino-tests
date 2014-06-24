34737c8ecca5401b04585e1afa2df34b3cf604f8
tigoe <tom.igoe@gmail.com>
2014-06-24 06:10:18 -0400

    changed DigitalInWatcher, added debounce

--- digitalInWatcher.js
+++ digitalInWatcher.js
@@ -3,15 +3,9 @@ var lastTime = 0;			// last time the button was pressed
 
 
 function buttonPress(event) { 
-	// get the time difference from the last button trigger:
-	var difference = event.time - lastTime;
-	// if it's less than 0.2 seconds:
-	if (difference > 0.2) {
 		console.log(event.time); 	// note the time
-		lastTime = event.time;		// store the event time for next comparison
-	}
 }
 
 // set a watcher for the button, and when it happens, 
 // trigger the buttonPress event:
-setWatch(buttonPress, buttonPin, {edge:"falling", repeat:true});
+setWatch(buttonPress, buttonPin, {edge:"falling", repeat:true, debounce:10});
